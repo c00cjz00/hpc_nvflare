4a446b98ce03a44d7c8ac087265f2d1e42b34e13
c00cjz00 <summerhill001@gmail.com>
2022-03-10 19:21:35 +0800

    Update condaInit.sh

--- hpc/condaInit.sh
+++ hpc/condaInit.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-## example bash condaInit.sh install_env nvflare 3.8
+## example source condaInit.sh install_env nvflare 3.8
 ## example source condaInit.sh use_env nvflare 3.8
 if [ "$1" = "install_env" ]
 then
 	## 安裝 nvflare
-    echo "install conda env"
+	echo "install conda env"
 	myENV="$2" #nvflare
 	python_version="$3" #3.8
 	myPackageHome=/work/$(whoami)/myenv
@@ -25,6 +25,12 @@ then
 	export PATH=${myPackageHome}/.package_${myENV}_nchc_conda/envs/{myENV}/lib/python${python_version}/site-packages:$PATH
 	export PATH=${myPackageHome}/.package_${myENV}_nchc_conda/envs/{myENV}/lib/python${python_version}/site-packages/bin:$PATH
 	conda create -n ${myENV} -y -c conda-forge mamba python=${python_version} ipykernel
+	conda activate ${myPackageHome}/.package_${myENV}_nchc_conda/envs/${myENV}
+	echo "use ipykerner:"
+	echo "python -m ipykernel install --user --name ${myENV}  --display-name '${myENV}'"
+	echo "use rkerner:"
+	echo "Rscript -e \"IRkernel::installspec(name = '${myENV}', displayname = '${myENV}')\"";
+
 elif [ "$1" = "use_env" ]
 then
 	## 使用 nvflare
@@ -45,9 +51,12 @@ then
 	export PATH=${myPackageHome}/.package_${myENV}_nchc_conda/envs/{myENV}/bin:$PATH
 	export PATH=${myPackageHome}/.package_${myENV}_nchc_conda/envs/{myENV}/lib/python${python_version}/site-packages:$PATH
 	export PATH=${myPackageHome}/.package_${myENV}_nchc_conda/envs/{myENV}/lib/python${python_version}/site-packages/bin:$PATH
-	conda activate ${myPackageHome}/.package_${myENV}_nchc_conda/envs/${myENV}	
+	conda activate ${myPackageHome}/.package_${myENV}_nchc_conda/envs/${myENV}
+	echo "use ipykerner:"
+	echo "python -m ipykernel install --user --name ${myENV}  --display-name '${myENV}'"
+	echo "use rkerner:"
+	echo "Rscript -e \"IRkernel::installspec(name = '${myENV}', displayname = '${myENV}')\"";    
 	#pip install nvflare numpy itk-io monai pandas kaleido plotly torch torchvision -U
 else
     echo "use use_env or install_env"
 fi
-
